4918cf9a76e4ec3d66ed4edac107a01aedf35119
pedro <troopper.san@gmail.com>
2024-12-24 11:15:28 -0300

    Adicionando o Terceiro projeto django de um blog

---
+++ accounts/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+class CustomUser(AbstractUser):
+    name = models.CharField(null=False,blank=False,max_length=255)
